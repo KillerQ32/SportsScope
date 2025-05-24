8a5d488d6abf7fe4f9a4f215a9b50f0f7cce412a
Quinten Ballard <68921864+KillerQ32@users.noreply.github.com>
2025-05-23 20:49:59 -0400

    Created PlayerList Dataframe

---
+++ testingPlayerlist.py
@@ -0,0 +1,48 @@
+import requests
+import pandas as pd
+
+url = "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLPlayerList"
+
+headers = {
+	"x-rapidapi-key": "ed5b5763f5msh60a32b281315712p165ec7jsne408d86bb9b9",
+	"x-rapidapi-host": "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers)
+
+#print(response.json())
+
+def create_player_list_dataframe():
+    data = response.json()
+    player_list = []
+    
+    for player in data["body"]:
+        player_info = {
+            "player_name": player.get("longName"),
+            "espn_name": player.get("espnName"),
+            "team": player.get("team"),
+            "jersey_number": player.get("jerseyNum"),
+            "age": player.get("age"),  # Safe access
+            "weight": player.get("weight"),
+            "height": player.get("height"),
+            "position": player.get("pos"),
+            "team_ID": player.get("teamID"),
+            "player_ID": player.get("playerID"),
+            "espn_ID": player.get("espnID"),
+            "headshot":player.get("espnHeadshot"),
+            "free_agent_status": player.get("isFreeAgent")
+        }
+        player_list.append(player_info)
+    
+    df = pd.DataFrame(player_list)
+    return df
+
+
+def main():
+    print("Testing DataFrame Creation")
+
+    player_list_dataframe = create_player_list_dataframe()
+    print(player_list_dataframe)
+
+main()
+
