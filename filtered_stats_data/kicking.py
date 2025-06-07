e85db9a2485f638532ca5bcb6643079f2a302f97
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-06-06 21:35:04 -0400

    Cleaned up filter stats data real quick

--- filtered_stats_data/kicking.py
+++ filtered_stats_data/kicking.py
@@ -1,7 +1,6 @@
 from player_stats_2022.kicking_stats_2022 import create_df_2022
 from player_stats_2023.kicking_stats_2023 import create_df_2023
 from player_stats_2024.kicking_stats_2024 import create_df_2024
-
 from utils.filtering import filter_df
 
 
@@ -20,8 +19,6 @@ kicking_2023 = filter_df(kicking_stats_2023, 2023, cols)
 kicking_2024 = filter_df(kicking_stats_2024, 2024, cols)
 
 
-print(kicking_2022.head())
-print(kicking_2023.head())
-print(kicking_2024.head())
-
-
+kicking_2022.head()
+kicking_2023.head()
+kicking_2024.head()
