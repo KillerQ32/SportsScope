0c162490c30b186c8b3016824d1418ba33346830
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-06-21 02:17:07 -0400

    tested insert players function
    fixed player_names.py import issues and inserted players to the db

--- filtered_stats_data/player_names.py
+++ filtered_stats_data/player_names.py
@@ -1,10 +1,12 @@
 from utils.filtering import combine_df
-from rushing import rush_player_list
-from receiving import rec_player_list
-from passing import passing_player_list
-from kicking import kicking_player_list
+from filtered_stats_data.rushing import rush_player_list
+from filtered_stats_data.receiving import rec_player_list
+from filtered_stats_data.passing import passing_player_list
+from filtered_stats_data.kicking import kicking_player_list
 
 
 def create_player_list():
     df = combine_df([rush_player_list,rec_player_list,passing_player_list,kicking_player_list])
     return df
+
+player_names = create_player_list()
