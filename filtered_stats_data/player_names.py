32aa555f0db3177ca97aff12c2cf4323bdf1220b
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-06-20 23:11:59 -0400

    added new folder for all database interactions
    new db folder will be dfs get inserted, queried, etc..

--- filtered_stats_data/player_names.py
+++ filtered_stats_data/player_names.py
@@ -2,12 +2,9 @@ from utils.filtering import combine_df
 from rushing import rush_player_list
 from receiving import rec_player_list
 from passing import passing_player_list
+from kicking import kicking_player_list
 
 
-
-combine_df([rush_player_list, passing_player_list, rec_player_list])
-print(rush_player_list)
-print(rec_player_list)
-print(passing_player_list)
-
-    
+def create_player_list():
+    df = combine_df([rush_player_list,rec_player_list,passing_player_list,kicking_player_list])
+    return df
