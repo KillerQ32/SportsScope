a40705b266ee9aba71cdd33225b6cf2a96f2e66c
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-06-25 20:00:42 -0400

    spaced out code a bit

--- db/insert_data.py
+++ db/insert_data.py
@@ -15,6 +15,7 @@ def insert_into_players_table(df):
 from filtered_stats_data.player_names import player_names
 insert_into_players_table(player_names)
 
+
 def insert_into_teams_table(df):
     insert_query = text("""
                         INSERT INTO teams (team_name, division, conference)
@@ -28,6 +29,7 @@ def insert_into_teams_table(df):
 from filtered_stats_data.teams import team_headers
 insert_into_teams_table(team_headers)
 
+
 def insert_into_rushing_table(df):
     insert_query = text("""
                         INSERT INTO rushing_stats (season_year, rush_attempts, rush_yards, rush_tds, longest_rush, games_played, team_name, player_name)
@@ -40,6 +42,7 @@ def insert_into_rushing_table(df):
         
 from filtered_stats_data.rushing import rush_player_stats
 insert_into_rushing_table(rush_player_stats)
+
         
 def insert_into_receiving_table(df):
     insert_query = text("""
@@ -50,9 +53,11 @@ def insert_into_receiving_table(df):
     with engine.connect() as conn:
         conn.execute(insert_query,df.to_dict(orient="records"))
         conn.commit()
+        
 from filtered_stats_data.receiving import rec_player_stats
 insert_into_receiving_table(rec_player_stats)
-        
+
+      
 def insert_into_passing_table(df):
     insert_query = text("""
                         INSERT INTO passing_stats (season_year, pass_completed, pass_attempts, pass_yards, pass_tds, pass_ints, pass_long, games_played, team_name, player_name)\
@@ -62,8 +67,10 @@ def insert_into_passing_table(df):
     with engine.connect() as conn:
         conn.execute(insert_query,df.to_dict(orient="records"))
         conn.commit()
+        
 from filtered_stats_data.passing import passing_player_stats
 insert_into_passing_table(passing_player_stats)
+
 
 def insert_into_kicking_table(df):
     insert_query = text("""
@@ -74,5 +81,6 @@ def insert_into_kicking_table(df):
     with engine.connect() as conn:
         conn.execute(insert_query,df.to_dict(orient="records"))
         conn.commit()
+        
 from filtered_stats_data.kicking import kicking_player_stats
 insert_into_kicking_table(kicking_player_stats)
