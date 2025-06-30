9ad544d5c9c45c4668a732a276e4351f50821e98
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-06-30 19:12:07 -0400

    added all the update queries for all stats table

--- db/update_foreign_keys.py
+++ db/update_foreign_keys.py
@@ -23,5 +23,73 @@ def update_rushing_foreign_keys():
         conn.execute(update_team_id)
         conn.execute(update_player_id)
     
-
 update_rushing_foreign_keys()
+
+def update_receiving_foreign_keys():
+    update_team_id = text("""
+                 UPDATE receiving_stats rs
+                 SET team_id = t.team_id
+                 FROM teams t
+                 WHERE rs.team_name = t.team_name
+                 AND rs.team_id IS NULL
+                 """)
+    
+    update_player_id = text("""
+                            UPDATE receiving_stats rs
+                            SET player_id = p.player_id
+                            FROM players p
+                            WHERE rs.player_name = p.player_name
+                            AND rs.player_id IS NULL
+                            """)
+    
+    with engine.begin() as conn:
+        conn.execute(update_team_id)
+        conn.execute(update_player_id)
+        
+update_receiving_foreign_keys()
+
+def update_passing_foreign_keys():
+    update_team_id = text("""
+                 UPDATE passing_stats ps
+                 SET team_id = t.team_id
+                 FROM teams t
+                 WHERE ps.team_name = t.team_name
+                 AND ps.team_id IS NULL
+                 """)
+    
+    update_player_id = text("""
+                            UPDATE passing_stats ps
+                            SET player_id = p.player_id
+                            FROM players p
+                            WHERE ps.player_name = p.player_name
+                            AND ps.player_id IS NULL
+                            """)
+    
+    with engine.begin() as conn:
+        conn.execute(update_team_id)
+        conn.execute(update_player_id)
+
+update_passing_foreign_keys()
+
+def update_kicking_foreign_keys():
+    update_team_id = text("""
+                 UPDATE kicking_stats ks
+                 SET team_id = t.team_id
+                 FROM teams t
+                 WHERE ks.team_name = t.team_name
+                 AND ks.team_id IS NULL
+                 """)
+    
+    update_player_id = text("""
+                            UPDATE kicking_stats ks
+                            SET player_id = p.player_id
+                            FROM players p
+                            WHERE ks.player_name = p.player_name
+                            AND ks.player_id IS NULL
+                            """)
+    
+    with engine.begin() as conn:
+        conn.execute(update_team_id)
+        conn.execute(update_player_id)
+
+update_kicking_foreign_keys()
