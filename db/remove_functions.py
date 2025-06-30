a10c7f6c931bf085b138a3bd88f61cc33a85428c
QBallardUMBC <uw94057@umbc.edu>
2025-06-30 19:33:16 -0400

    Created remove_functions file for database removal; removed player_name and team_name columns from '_stats' files

---
+++ db/remove_functions.py
@@ -0,0 +1,56 @@
+from db.engine import engine
+from sqlalchemy import text
+
+"""
+This file is made for removing any data from the database
+"""
+
+def remove_player_names_rushing_stats():
+    #Drop player_name and team_name columns from rushing_stats table
+    delete_query = text("""
+        ALTER TABLE rushing_stats
+        DROP COLUMN player_name,
+        DROP COLUMN team_name
+    """)
+
+    with engine.begin() as conn:
+        conn.execute(delete_query)
+
+def remove_player_names_kicking_stats():
+    #Drop player_name and team_name columns from kicking_stats table
+    delete_query = text("""
+        ALTER TABLE kicking_stats
+        DROP COLUMN player_name,
+        DROP COLUMN team_name
+    """)
+
+    with engine.begin() as conn:
+        conn.execute(delete_query)
+
+def remove_player_names_receiving_stats():
+    #Drop player_name and team_name columns from receiving_stats table
+    delete_query = text("""
+        ALTER TABLE receiving_stats
+        DROP COLUMN player_name,
+        DROP COLUMN team_name
+    """)
+
+    with engine.begin() as conn:
+        conn.execute(delete_query)
+
+def remove_player_names_passing_stats():
+    #Drop player_name and team_name columns from passing_stats table
+    delete_query = text("""
+        ALTER TABLE passing_stats
+        DROP COLUMN player_name,
+        DROP COLUMN team_name
+    """)
+
+    with engine.begin() as conn:
+        conn.execute(delete_query)
+
+if __name__ == "__main__":
+    remove_player_names_rushing_stats()
+    remove_player_names_receiving_stats()
+    remove_player_names_passing_stats()
+    remove_player_names_kicking_stats()
