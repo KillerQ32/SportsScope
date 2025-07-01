82024fcece5d3d0d3afdd847b54cda3520bdf153
QBallardUMBC <uw94057@umbc.edu>
2025-07-01 19:44:38 -0400

    Created search players stats by season year and name

--- backend/queries/passing_stats_queries.py
+++ backend/queries/passing_stats_queries.py
@@ -7,3 +7,11 @@ def player_passing_stats():
                 on p.player_id = ps.player_id
                 WHERE LOWER(p.player_name) = LOWER(:player_name)""")
     return query
+
+def player_passing_stats_year():
+    query = text("""select p.player_id, p.player_name, p.position, ps.pass_completed, ps.pass_attempts, ps.pass_yards, ps.pass_tds, ps.pass_ints, ps.pass_long, ps.season_year
+                FROM passing_stats ps
+                join players p 
+                on p.player_id = ps.player_id
+                WHERE LOWER(p.player_name) = LOWER(:player_name) AND ps.season_year = :season_year""")
+    return query
--- backend/queries/rushing_stats_queries.py
+++ backend/queries/rushing_stats_queries.py
@@ -16,3 +16,11 @@ def player_rushing_stats():
                 on p.player_id = rs.player_id
                 WHERE LOWER(p.player_name) = LOWER(:player_name)""")
     return query
+
+def player_rushing_stats_year():
+    query = text("""select p.player_id, p.player_name, p.position, rs.rush_attempts, rs.rush_yards, rs.rush_tds, rs.longest_rush, rs.season_year
+                FROM rushing_stats rs
+                join players p 
+                on p.player_id = rs.player_id
+                WHERE LOWER(p.player_name) = LOWER(:player_name) AND rs.season_year = :season_year""")
+    return query
