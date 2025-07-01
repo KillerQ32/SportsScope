b44d84707d78c88b0d79f75c85cfb92f04d30f5a
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-07-01 16:33:58 -0400

    added query method for more param validation

--- backend/routes/players.py
+++ backend/routes/players.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from backend.db.database import get_db
 from backend.queries.players_queries import all_players
+
 
 router = APIRouter()
 
 @router.get("/")
-def get_players(limit: int = 100,db=Depends(get_db)):
+def get_players(limit: int = Query(50, ge=1, le=970),db=Depends(get_db)):
     query = all_players(limit)
     result = db.execute(query)
     return [dict(row._mapping) for row in result]
