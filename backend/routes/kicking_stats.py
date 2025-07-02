30aa505dad9dfdd281334f026eb591d680a255c6
QBallardUMBC <uw94057@umbc.edu>
2025-07-01 20:25:24 -0400

    search by player and year for kicking_stats

--- backend/routes/kicking_stats.py
+++ backend/routes/kicking_stats.py
@@ -9,3 +9,15 @@ def get_fg_made(min_fg_made: int = Query(10,ge=0),db=Depends(get_db)):
     query = ks.kicking_stats()
     result = db.execute(query,{"min_fg_made": min_fg_made})
     return [dict(row._mapping) for row in result]
+
+@router.get("/players")
+def get_player_fg_made(player_name: str, db=Depends(get_db)):
+    query = ks.player_kicking_stats()
+    result = db.execute(query, {"player_name": player_name})
+    return [dict(row._mapping) for row in result]
+
+@router.get("/players/{season_year}")
+def get_player_fg_made_year(player_name: str, season_year: int, db=Depends(get_db)):
+    query = ks.player_kicking_stats_year()
+    result = db.execute(query, {"player_name": player_name, "season_year": season_year})
+    return [dict(row._mapping) for row in result]
