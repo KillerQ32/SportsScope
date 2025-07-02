faf217b73fa7f4fa2281d5f952c0d818a5a83138
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-07-01 20:45:33 -0400

    literal white space deletion

--- backend/routes/rush_stats.py
+++ backend/routes/rush_stats.py
@@ -7,9 +7,8 @@ import backend.queries.rushing_stats_queries as rs
 router = APIRouter()
 
 @router.get("/tds")
-def get_tds(min_tds: int = Query(0, ge=0), db=Depends(get_db)):
-    """ return top rushing players sorted by tds. defualt is 0 or more tds"""
-    
+def get_tds(min_tds: int = Query(1, ge=0), db=Depends(get_db)):
+    """ return top rushing players sorted by tds. defualt is 1 or more tds"""
     query = rs.most_tds()
     result = db.execute(query, {"min_tds": min_tds})
     return [dict(row._mapping) for row in result]
