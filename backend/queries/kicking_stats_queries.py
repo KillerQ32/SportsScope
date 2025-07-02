8797feab08765960a812775e3d87e9aafb371f25
Jesse Ankrah <Ankrahjesse@gmail.com>
2025-07-01 20:15:16 -0400

    edited kicking query

--- backend/queries/kicking_stats_queries.py
+++ backend/queries/kicking_stats_queries.py
@@ -1,12 +1,13 @@
 from sqlalchemy import text
 
-def kicking_stats(limit):
+def kicking_stats():
     query = text(f"""
                  select p.player_name, k.fg_attempts, k.fg_made, k.xp_attempts,k.xp_made,k.season_year
                 from kicking_stats k
                 join players p 
                 on p.player_id = k.player_id
+                Where k.fg_made >= :min_fg_made
                 order by k.fg_made desc
-                limit {limit};
+                ;
                  """)
     return query
--- backend/routes/kicking_stats.py
+++ backend/routes/kicking_stats.py
@@ -5,7 +5,7 @@ import backend.queries.kicking_stats_queries as ks
 router = APIRouter()
 
 @router.get("/fg_made")
-def get_fg_made(limit: int = Query(10,ge=0),db=Depends(get_db)):
-    query = ks.kicking_stats(limit)
-    result = db.execute(query)
+def get_fg_made(min_fg_made: int = Query(10,ge=0),db=Depends(get_db)):
+    query = ks.kicking_stats()
+    result = db.execute(query,{"min_fg_made": min_fg_made})
     return [dict(row._mapping) for row in result]
